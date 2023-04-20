7564200355450d33647a0a687de97ce110c39115
Mustafa Abdulle <mustafa.abdulle@student.nbi-handelsakademin.se>
2023-04-20 19:08:55 +0200

    login F-E

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,40 @@
+
+// =============== PROFILE =============== //
+
+const userBtn = document.getElementById('user-btn');
+const loginRegisterSection = document.querySelector('.login-register');
+
+userBtn.addEventListener('click', function() {
+  loginRegisterSection.classList.toggle('hidden');
+  if (loginRegisterSection.classList.contains('hidden')) {
+    // remove the color style
+    userBtn.style.color = ''; 
+  } else {
+    // set the color style
+    userBtn.style.color = '#3B8419'; 
+  }
+});
+
+
+// =============== LOGIN/REGISTER =============== //
+
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login__link');
+const registerLink = document.querySelector('.register__link');
+
+registerLink.addEventListener('click', () =>{
+    wrapper.classList.add('active');
+})
+loginLink.addEventListener('click', () =>{
+    wrapper.classList.remove('active');
+})
+
+function goBack() {
+    cartBtn.style.backgroundColor = '#3E464E';
+    window.history.back();
+}
+
+
 /* =============== CART BAR =============== */
 
 const cartItemContainer = document.querySelector('.cart-item-container');
